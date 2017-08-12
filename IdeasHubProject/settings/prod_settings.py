f40dfdf1629f7796931a4513a84716023a87b4ed
Derek Lopes <derkle@live.com>
2017-08-12 16:47:34 -0700

    Created settings files for different environments

---
+++ IdeasHubProject/settings/prod_settings.py
@@ -0,0 +1,24 @@
+"""
+Setting for prod.
+
+Used in deployment to PythonAnywhere.
+"""
+
+from base_settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/dev/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'derkle$ideashub',
+        'USER': 'derkle',
+        'PASSWORD': 'sjsu100w',
+        'HOST': 'derkle.mysql.pythonanywhere-services.com'
+    }
+}
+
